ae064f1385f4409ed671f2ecaa2df380a3b29123
kazuyama8050 <kazuki118050@gmail.com>
2022-11-06 23:11:06 +0900

    first_commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getTest()
+    {
+        $test = 'tests_json';
+        return json_encode($test);
+    }
+}
