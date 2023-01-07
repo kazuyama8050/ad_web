ae0dd098e6609a286d5a53eda36205a9d8ccf43e
kazuyama8050 <kazuki118050@gmail.com>
2023-01-07 17:05:43 +0900

    管理画面home

---
+++ resources/js/views/admin/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+    <HomeMenuComponent :menuObject="menuObject"></HomeMenuComponent>
+</template>
+<script>
+import Vue from "vue";
+import HomeMenuComponent from "../../components/common/HomeMenuComponent.vue";
+
+const menuObject = {
+    'advertise':  '広告',
+    'deliveler': 'アフィリエイター',
+    'advertiser': '広告主',
+};
+export default {
+    name: "AdminHome",
+    components: {
+        HomeMenuComponent,
+    },
+    data: function() {
+        return {
+            menuObject,
+        }
+    },
+}
+</script>
