7519fd4f6bea28d0253522942bfb6825586b2ec5
kazuyama8050 <kazuki118050@gmail.com>
2023-01-07 17:05:08 +0900

    共通コンポーネント追加

---
+++ resources/js/components/common/TabComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="nav nav-tabs">
+        <li class="nav-item" v-for="(value, path) in tabObject">
+            <a class="nav-link active" aria-current="page" href="#" v-if="activePage == path">{{ value }}</a>
+            <a class="nav-link text-reset" :href="path" v-if="activePage != path">{{ value }}</a>
+        </li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: "TabComponent",
+        props: {
+            tabObject: {
+                type: Object,
+                required: true,
+            },
+            activePage: {
+                type: String,
+                required: true,
+            },
+        },
+    }
+</script>
---
+++ resources/js/components/form/FormTextComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="mb-3">
+        <label :for="inputName" class="form-label">{{ thValue }}<span :class="spanClass">{{ spanValue }}</span></label>
+        <input :type="inputType" class="form-control" :id="inputName">
+    </div>
+</template>
+<script>
+    export default {
+        name: "FormTextComponent",
+        props: {
+            thValue: {
+                type: String,
+                required: true,
+            },
+            spanValue: {
+                type: String,
+                required: false,
+                default: "必須",
+            },
+            textSize: {
+                type: String,
+                required: false,
+                default: "20",
+            },
+            inputName: {
+                type: String,
+                required: true,
+            },
+            inputType: {
+                type: String,
+                required: false,
+                default: "text",
+            },
+        },
+        setup(props) {
+            const spanValue = props.spanValue;
+            var spanClass = "hissu";
+            if (spanValue == null) {
+                spanClass = "display-none";
+            }
+            return {
+                spanClass,
+            }
+        }
+    }
+</script>
